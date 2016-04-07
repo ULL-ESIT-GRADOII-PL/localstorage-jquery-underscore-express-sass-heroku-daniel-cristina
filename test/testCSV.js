61f88b3cbbc1166507ce93b0d0dfd532357d6c6c
alu0100821338 <alu0100821338@ull.edu.es>
2016-04-07 17:23:04 +0100

    Añadiendo test y directorio vendor

--- test/testCSV.js
+++ test/testCSV.js
@@ -1,12 +1,17 @@
 var expect = chai.expect;
 
 
-
-
  describe("Calculate CSV", function() {
-    it('Debería devolover un string', function() {
-        textArea.value = '"Esto", "Devuelve" \n "Un", "String"';
-        calculate();
-        assert.isString(finaltable.innerHTML);
+    it('Debería reconocer una string', function() {
+      var entrada = '"producto", "precio"';
+      var r = calculate(entrada);
+     expect(r[0].value[1]).to.equal('precio');
     });
+
+    it('Debería reconocer un numero con coma', function() {
+      var entrada = '"2,7",1,hola';
+      var r = calculate(entrada);
+     expect(r[0].value[0]).to.equal('2,7');
+    });
+
 });
