2ccb80978feb0fd376695b02a5c3478e3bf21096
alu0100811933 <alu0100811933@ull.edu.es>
2016-04-09 13:10:32 +0100

    procfile y app.js modificados

---
+++ karma.conf.js
@@ -0,0 +1,66 @@
+// Karma configuration
+
+module.exports = function(config) {
+  config.set({
+
+    basePath: '',
+
+    frameworks: ['mocha', 'chai'],
+
+    files: [
+      'test/testCSV.js',
+      'vendor/chai.js',
+      'vendor/mocha.css',
+      'vendor/mocha.js',
+      'vendor/blanket.js',
+      'vendor/mocha-blanket.js',
+      'csv.js'
+    ],
+
+    client: {
+          mocha: {
+            ui: 'bdd'
+          }
+    },
+
+    exclude: [
+      'gulpfile.js'
+    ],
+
+    preprocessors: {
+      'test/test.html': ['html2js']
+    },
+
+    plugins : [
+      'karma-mocha',
+      'karma-chai',
+      'karma-firefox-launcher',
+      'karma-html2js-preprocessor',
+      'karma-phantomjs-launcher',
+      'karma-chrome-launcher'
+    ],
+
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+
+    reporters: ['progress'],
+
+    port: 9876,
+
+    colors: true,
+
+    logLevel: config.LOG_INFO,
+
+    autoWatch: true,
+
+    browsers: ['Firefox', 'Chrome_travis_ci', 'PhantomJS'],
+
+    captureTimeout: 60000,
+
+    singleRun: false
+  });
+};
